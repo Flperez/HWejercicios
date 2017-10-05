d1bcb64d8b6991d033bf5bb733168667c49545e2
f <felipeperez2710@gamil.com>
2017-10-05 20:41:02 +0200

    terminado

--- ejercicio7.py
+++ ejercicio7.py
@@ -3,6 +3,12 @@ import argparse
 import glob, os
 import cv2
 from collections import deque
+import re
+numbers = re.compile(r'(\d+)')
+def numericalSort(value):
+    parts = numbers.split(value)
+    parts[1::2] = map(int, parts[1::2])
+    return parts
 
 #funciones
 def nueva_ruta(ruta_img,path_out):
@@ -35,12 +41,15 @@ if __name__ == "__main__":
     altos = np.array([88, 255, 255], dtype=np.uint8)
 
     kernel = np.ones((3, 3), np.uint8)
-
-    for infile in sorted(glob.glob((path_in+'/*jpg'))):
+    num = 10
+    colax = deque(maxlen=num)
+    colay = deque(maxlen=num)
+    cont = 0
+    for infile in sorted(glob.glob((path_in+'/*jpg')),key=numericalSort):
         file, ext = os.path.splitext(infile)
         print("Procesando: ",infile)
         im = cv2.imread(infile)
-        hsv=cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv,bajos,altos)
 
         #Erosion
@@ -49,35 +58,45 @@ if __name__ == "__main__":
         #dilatacion
         cv2.dilate(src=mask,dst=mask, kernel=kernel, iterations=2)
 
-        edges = cv2.Canny(mask, 1, 2)
-        _,contours, hier = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #Calculo del area maxima
+        _,contours, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         areas = [cv2.contourArea(c) for c in contours]
 
-        max = np.amax(areas)
-        j= np.argmax(areas)
 
-        #cv2.drawContours(im,[contours[j]],0,(0,0,255),2)
-        #cv2.drawContours(mask, [contours[j]], 0, (0, 0, 255), 2)
 
-        mu = cv2.moments(contours[j])
-        x = int(mu['m10']/mu['m00'])
-        y = int(mu['m01']/mu['m00'])
+        j = np.argmax(areas) #indice del contorno con mayor area
+        cv2.drawContours(mask,contours,j,(255,0,0))
 
-        _,radius = cv2.minEnclosingCircle(contours[j])
+
+        (x,y),radius = cv2.minEnclosingCircle(contours[j])
         radius = int(radius)
+        center =(int(x),int(y))
+        cv2.circle(im, center, radius, (0, 0, 255), 2)
+        cv2.circle(mask, center, radius, (0, 0, 255), 2)
 
-        cv2.circle(im,(x,y),radius,(0,0,255),2)
 
-        resized = cv2.resize(im, (0, 0), fx=0.5, fy=0.5)
-        cv2.imshow("mask",mask)
-        cv2.imshow("image",resized)
-        cv2.waitKey(100)
-
-        #pintar trayectoria
-        #cola circular
+        # pintar trayectoria
+        ## cola circular
         #actualizar cola
-        #for para pintar
+        colax.append(center[0])
+        colay.append(center[1])
+        cont=cont+1
 
+        #Se han rellenado los 10 valores
+        if cont>colax.maxlen:
+            for k in range(0,colax.maxlen-1):
+                cv2.line(im,(colax[k],colay[k]),(colax[k+1],colay[k+1]),(255,0,0),k)
+
+
+
+
+        #for para pintar
+        r1 = cv2.resize(im, (0, 0), fx=0.5, fy=0.5)
+        r2 = cv2.resize(mask, (0, 0), fx=0.5, fy=0.5)
+
+        cv2.imshow("mask", r2)
+        cv2.imshow("image", r1)
+        cv2.waitKey(100)
 
     cv2.destroyAllWindows()
 
