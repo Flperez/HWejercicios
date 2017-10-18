89914aad57524f8d90b348512d96424d750d18d9
f <felipeperez2710@gamil.com>
2017-10-18 11:20:54 +0200

    Listo para entregar

--- mediamatcher.py
+++ mediamatcher.py
@@ -2,22 +2,29 @@ import numpy as np
 import cv2
 import argparse
 import glob
+import matplotlib.pyplot as plt
 
-N = 10
+N = 10 #umbral
 
-def  numMatches(rutaImgScene,des1,descriptor):
+def  numMatches(rutaImgScene,des1,detector):
     '''
+    codigo copiado del ejemplo:
+    https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_matcher/py_matcher.html#matcher
+
+    Funcion que recibe la ruta a la imagen con la que queremos comparar junto con el
+    descriptor de la imagen plantilla y el detector que vamos a usar
 
     :param rutaImgScene: ruta a la imagen del dataset
     :param des1: descriptor de la primera imagen
-    :param descriptor: algoritmo que usaremos para extraer los puntos de interes
+    :param detector: algoritmo que usaremos para extraer los puntos de interes
     :return: numero de matches
     '''
+
     img2 = cv2.imread(rutaImgScene, 0)  # queryImage
 
 
     # find the keypoints and descriptors with SIFT
-    kp2, des2 = descriptor.detectAndCompute(img2, None)
+    kp2, des2 = detector.detectAndCompute(img2, None)
 
     # FLANN parameters
     FLANN_INDEX_KDTREE = 0
@@ -32,13 +39,14 @@ def  numMatches(rutaImgScene,des1,descriptor):
     matchesMask = [[0, 0] for i in range(len(matches))]
 
     # ratio test as per Lowe's paper
-    cont = 0
+    num_matches = 0
     for i, (m, n) in enumerate(matches):
         if m.distance < 0.7 * n.distance:
-            matchesMask[i] = [1, 0]
-            cont = cont + 1
+            num_matches = num_matches+1
 
-    return cont
+    print("Imagen: ",rutaImgScene,
+          " con ",num_matches," matches")
+    return num_matches
 
 #paso de argumentos
 ap = argparse.ArgumentParser()
@@ -53,7 +61,9 @@ rutaImgCovers = args['covers']
 
 if __name__ == "__main__":
 
-    img1 = cv2.imread(rutaImgObject, 0)  # queryImage
+    img = cv2.imread(rutaImgObject)  # queryImage
+    img1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
 
 
     # Initiate SIFT detector
@@ -62,12 +72,30 @@ if __name__ == "__main__":
     # find the keypoints and descriptors with SIFT
     _, des1 = sift.detectAndCompute(img1, None)
 
-    infile = sorted(glob.glob((rutaImgCovers+'/*png')))
-    list = [numMatches(rutaImgScene=rut,des1=des1,descriptor=sift) for rut in infile]
+    infile = sorted(glob.glob((rutaImgCovers+'/*jpg')))
+    list = [numMatches(rutaImgScene=rut,des1=des1,detector=sift) for rut in infile]
     arg_max = int(np.argmax(list))
 
     if list[arg_max]>N:
-        print(infile[arg_max])
+        # mostramos los resultados
+        print("\nLa imagen que mas se asemeja a la introducida es: \n ",
+              infile[arg_max]," con ",list[arg_max]," matches")
+
+
+        img2 = cv2.imread(infile[arg_max])
+
+        plt.subplot(121)
+        img = cv2.cvtColor(src=img, code=cv2.COLOR_BGR2RGB)
+        plt.imshow(img)
+        plt.title("imagen original")
+        plt.subplot(122)
+        img2 = cv2.cvtColor(src=img2, code=cv2.COLOR_BGR2RGB)
+        plt.imshow(img2)
+        plt.title("imagen correspondiente")
+        plt.show()
+
+
+
     else:
         print("No hay ninguna imagen en la lista que supere el umbral: ",N)
 
