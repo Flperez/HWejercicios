6d5dd28f8fc3a71b12505ae479ced01cf6989952
f <felipeperez2710@gamil.com>
2017-10-21 19:45:17 +0200

    listo para entregar

--- eyefacedetector.py
+++ eyefacedetector.py
@@ -1,24 +1,22 @@
 import cv2
-#import numpy as np
 import argparse
 
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-v","--video",required = False, help = "ruta a donde guarda el archivo de video de entrada")
-ap.add_argument("-o","--out",required = False, help = "ruta a donde guarda el archivo de video resultante")
+ap.add_argument("-v","--video",required = True, help = "ruta a donde guarda el archivo de video de entrada")
+ap.add_argument("-o","--out",required = True, help = "ruta a donde guarda el archivo de video resultante")
 
 
 args = vars(ap.parse_args())
 
 path_in = args['video']
 path_out = args['out']
-path_cascade_face = '/usr/local/share/OpenCV/haarcascades/haarcascade_frontalcatface.xml'
-path_cascade_eyes = '/usr/local/share/OpenCV/haarcascades/haarcascade_eye.xml'
 
 
-faceCascade = cv2.CascadeClassifier(path_cascade_face)
-eyesCascade = cv2.CascadeClassifier(path_cascade_eyes)
+
+faceCascade = cv2.CascadeClassifier('/home/f/PycharmProjects/HWejercicios/haarcascade_frontalface_default.xml')
+eyesCascade = cv2.CascadeClassifier('/home/f/PycharmProjects/HWejercicios/haarcascade_eye.xml')
 
 
 #cargamos video de entrada
@@ -26,33 +24,33 @@ cap = cv2.VideoCapture(path_in)
 #video de salida
 fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I','D')
 out = cv2.VideoWriter(path_out,fourcc, 33.0, (640,480))
-
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        frame = cv2.flip(frame, 0)
-        frame_original = frame.copy()
-        faces = faceCascade.detectMultiScale(frame,
+        frame_gray = cv2.cvtColor(frame,code=cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(frame_gray,
                                              scaleFactor=1.1,
                                              minNeighbors=5,
                                              flags=cv2.CASCADE_SCALE_IMAGE)
         for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (125, 255, 0), 2)
-            eyes = eyesCascade.detectMultiScale(frame,
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            eyes = eyesCascade.detectMultiScale(frame_gray,
                                                 scaleFactor=1.1,
                                                 minNeighbors=5,
                                                 minSize=(30, 30),
                                                 flags=cv2.CASCADE_SCALE_IMAGE)
             for (x, y, w, h) in eyes:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (125, 255, 0), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
         out.write(frame)
         cv2.imshow('frame', frame)
+        key = cv2.waitKey(33)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if key == 113: #letra q
             break
-        else:
-            break
+    else:
+        break
 
 cap.release()
 out.release()
